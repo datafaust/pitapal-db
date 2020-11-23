be4ba12667d509b85e9efa4d90eb08b073152587
mac <mac@macs-MBP.fios-router.home>
2020-11-22 23:07:54 -0500

    fix key issue

--- node/routes/mealKey.routes.js
+++ node/routes/mealKey.routes.js
@@ -1,5 +1,5 @@
 module.exports = app => {
-    const mealKey = require("../controllers/mealkey.controller.js");
+    const mealKey = require("../controllers/mealKey.controller.js");
   
     // Create a new Customer
     app.post("/mealKey", mealKey.create);
